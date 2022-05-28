3db8a2288b49eea1dfcabf513243e5dfbe5c635e
Fabio Pinatti <fpinatti@gmail.com>
2022-05-28 09:42:19 -0300

    verison code

--- maritaca/app.config.js
+++ maritaca/app.config.js
@@ -20,7 +20,7 @@ export default {
     ],
     "android": {
       "package": "com.pinapps.maritaca",
-      "versionCode": 4,
+      "versionCode": 5,
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#C742A1"
